0f35b5514578c2dfe53eb7fe88f6072dddd22199
marcoweb <marcocarvalho.web@gmail.com>
2022-10-05 17:56:40 +0000

    Update

--- app/src/main/java/marcocarvalho/controllers/AlunosController.java
+++ app/src/main/java/marcocarvalho/controllers/AlunosController.java
@@ -1,8 +1,11 @@
 package marcocarvalho.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,5 +36,12 @@ public class AlunosController {
         aluno.setIdade(idade);
         alunosRepo.save(aluno);
         return "redirect:/alunos/list";
+    }
+
+    @RequestMapping("update/{id}")
+    public String update(Model model, @PathVariable int id) {
+        Optional<Aluno> aluno = alunosRepo.findById(id);
+        model.addAttribute("aluno", aluno.get());
+        return "/alunos/update";
     }
 }
